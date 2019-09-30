cee73417f78cca7a899753b7a8cf1d0bd6f3e7f9
Niloy <md.niloy26643@gmail.com>
2019-09-30 23:18:20 +0600

    first commit

---
+++ Fibo.py
@@ -0,0 +1,7 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=56
+>>> b=76
+>>> print(a+b)
+132
+>>> 
